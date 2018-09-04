d855d4970f0b656c1f093047e5d61ea6964593a6
Fernando C Ribeiro <fernando@contabilizei.com.br>
2018-09-04 01:11:22 -0300

    Versao com Visual Studio

---
+++ WebApplication1/Repository/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Seguradora.Model;
+using System.Collections.Generic;
+
+namespace Seguradora.Repository
+{
+    public interface IUserRepository
+    {
+        User GetById(long id);
+        User GetByAuth(Login auth);
+        HashSet<User> GetAll();
+        void Delete(long id);
+        User Save(User user);
+    }
+}
---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.EntityFrameworkCore;
+using Seguradora.Models;
+using Seguradora.Repository;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = Configuration["Jwt:Issuer"],
+                ValidAudience = Configuration["Jwt:Issuer"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+            };
+        });
+
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SeguradoraContext>(options =>
+                    options.UseMySql(Configuration.GetConnectionString("SeguradoraContext")));
+
+            // Configurando o serviço de documentação do Swagger
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1",
+                    new Info
+                    {
+                        Title = "Seguradora API",
+                        Version = "v1",
+                        Description = "Demonstração projeto .netcore 2.1",
+                        Contact = new Contact
+                        {
+                            Name = "Fernando Colnaghi Ribeiro",
+                            Url = "https://github.com/fcolnaghi",
+                            Email = "fernando.colnaghi@gmail.com"
+                        }
+                    });
+                // Swagger 2.+ support
+                var security = new Dictionary<string, IEnumerable<string>>
+                {
+                    {"Bearer", new string[] { }},
+                };
+
+                c.AddSecurityDefinition("Bearer", new ApiKeyScheme
+                {
+                    Description = "JWT Authorization. gerar o Token no endpoint token, apos autenticar os servicos \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = "header",
+                    Type = "apiKey",
+                });
+
+                c.AddSecurityRequirement(security);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Seguradora API");
+            });
+        }
+    }
+}
